4af9f91a2926782a41eb2755930dbe4f3c2f40b5
heyaryan001 <hey.itss.aryan@gmail.com>
2024-08-11 15:40:28 +0530

    Update path for assets

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Github-card-generator/",
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Listen on all IP addresses
     port: 5173       // You can specify the port
-  }
+  },
+  base: "/Github-card-generator/"
   
 })
